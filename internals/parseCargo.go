10130d9a07449f9f0cadc587a975b974abeda1e7
JT <Jordantay9014@outlook.com>
2023-04-08 21:35:33 -0500

    GetCrateURL - working

--- internals/parseCargo.go
+++ internals/parseCargo.go
@@ -2,7 +2,8 @@ package internals
 
 import (
 	"context"
-	"fmt"
+	"log"
+
 	"github.com/google/go-github/v37/github"
 	"golang.org/x/oauth2"
 )
@@ -10,35 +11,47 @@ import (
 // Set up a new GitHub client with an access token
 var ctx = context.Background()
 var ts = oauth2.StaticTokenSource(
-	&oauth2.Token{AccessToken: "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"},
+	&oauth2.Token{AccessToken: "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"},
 )
 var tc = oauth2.NewClient(ctx, ts)
 var client = github.NewClient(tc)
 
-func ParseCargo() {
-
+func GetCrateUrl(fileName string) string {
 	// Get the contents of the "crates" folder
+	crateUrl := ""
 	_, dirContents, _, err := client.Repositories.GetContents(ctx, "justjordant", "cargo-crates", "Crates", &github.RepositoryContentGetOptions{})
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// Iterate through the contents of the "crates" folder
 	for _, content := range dirContents {
-		if content.GetType() == "dir" {
-			// If the content is a directory, get its contents as well
-			_, subDirContents, _, err := client.Repositories.GetContents(ctx, "justjordant", "cargo-crates", content.GetPath(), &github.RepositoryContentGetOptions{})
-			if err != nil {
-				panic(err)
-			}
+		//  if content.GetType() == "dir" {
+		//  // If the content is a directory, get its contents as well
+		//  _, subDirContents, _, err := client.Repositories.GetContents(ctx, "justjordant", "cargo-crates", content.GetPath(), &github.RepositoryContentGetOptions{})
+		//  if err != nil {
+		//  panic(err)
+		//  }
 
-			// Print the names of the files in the subdirectory
-			for _, subContent := range subDirContents {
-				fmt.Println(subContent.GetName())
-			}
-		} else {
-			// If the content is a file, print its name
-			fmt.Println(content.GetName())
+		//  // Print the names of the files in the subdirectory
+		//  for _, subContent := range subDirContents {
+		//  fmt.Println(subContent.GetName())
+		//  }
+		//  } else {
+		// If the content is a file, print its name
+		//  }
+		//  fmt.Println(content.GetName())
+		//  fmt.Println(content.GetType())
+		//  fmt.Println(content.GetURL())
+		//  fmt.Println(content.GetGitURL())
+		//  fmt.Println(content.GetPath())
+		//  fmt.Println("\n")
+		if fileName == content.GetName() {
+			crateUrl = content.GetURL()
 		}
 	}
+	return crateUrl
+}
+
+func downloadCargoYaml(fileName string) {
 }
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/justjordant/cargo/internals"
+import (
+	"fmt"
+
+	"github.com/justjordant/cargo/internals"
+)
 
 func main() {
-	internals.ParseCargo()
+	fmt.Println(internals.GetCrateUrl("cli-ops.yml"))
 	// TODO Parse need to be cleaned up
 	// TODO Need to pass in what file we want to download from the `Crates` to then be installed down the pipe
 }
