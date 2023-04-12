ee0f275fae0b112679ed60d53fe55ca382e61cb8
JT <Jordantay9014@outlook.com>
2023-04-12 09:29:41 -0500

    download crate yaml wokring with sudo -E

--- main.go
+++ main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/justjordant/cargo/cmd"
+	"github.com/justjordant/cargo/internals"
 	"github.com/justjordant/cargo/internals/initUtils"
 )
 
 func main() {
 
+	// TODO - This section might be better off in BASH, where we can pull in the all the needed this.
 	createFile := false
 	if createFile {
 		initUtils.InitDirs()
 	}
 	cmd.Execute()
 
-	//  url, fileName := internals.GetCrateUrl("tool.yml")
-	//  fmt.Println(url)
-	//  fmt.Println(internals.DownloadCargoYaml(url, internals.FormatFileName(fileName)))
+	url, fileName := internals.GetCrateUrl("tool.yml")
+	fmt.Println(url)
+	fmt.Println(internals.DownloadCargoYaml(url, internals.FormatFileName(fileName)))
 	// TODO Parse need to be cleaned up
 	// TODO Need to pass in what file we want to download from the `Crates` to then be installed down the pipe
 }
